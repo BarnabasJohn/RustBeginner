798be1fff84ca8b359f5673cedd928154a6748d3
BarnabasJohn <barnoz7394@gmail.com>
2024-07-25 06:17:58 +0300

    w

---
+++ Rust's Module System/restaurant/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+mod front_house;
+
+pub use crate::front_house::hosting;
+
+pub fn eat_at_restaurant () {
+    hosting::add_to_waitlist();
+}
+
+
+
